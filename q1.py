405131c601c0a7b812d8a023c2c335d0b28765a2
Joel Sved <joelsved123@gmail.com>
2023-01-11 09:56:57 +1100

    first_attempt

--- q1.py
+++ q1.py
@@ -1,3 +1,15 @@
-# Project Euler Question 1
+# Project Euler Question 1 :: Find the sum of all multiples of 3 or 5 below 1000
+# Joel Sved
+import numpy as np 
 
-print('Hello World!')
+num_arr = np.arange(1,1001)
+multiples_list = []
+
+for i in range(len(num_arr)):
+    if (i%3 == 0 or i%5 == 0):
+        multiples_list = np.append(multiples_list,i)
+        print(i)
+print(np.sum(multiples_list))        
+
+
+
